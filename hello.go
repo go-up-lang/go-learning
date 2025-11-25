9e91422b2fe507b23c190b718799bf08ec8cf845
go-up-lang <dbsrbtjs@gmail.com>
2025-11-25 23:45:57 +0900

    go-language1

---
+++ hello.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func addFor(numbers ...int) int {
+	for number := range numbers {
+		fmt.Println(number)
+	}
+	return 0
+}
+
+func main() {
+	addFor(1, 2, 3, 4, 5, 6)
+}
+
+//// 1. function1 -------------------------
+// func lenAn(name string) (int, string) {
+// 	return len(name), string(name)
+// }
+
+// func main() {
+// 	totalLenght, upperName := lenAn("nico")
+// 	fmt.Println(totalLenght, upperName)
+// }
+
+//// 2. function2 -------------------------
+// func lenAn(name string) (lenght int, uppercase string) {
+// 	lenght = len(name)
+// 	uppercase = strings.ToUpper(name)
+// 	return
+// }
+
+// func main() {
+// 	totalLenght, up := lenAn("nico")
+// 	fmt.Println(totalLenght, up)
+// }
+
+// // 3. function defer -------------------------
+// func lenAn(name string) (lenght int, uppercase string) {
+// 	defer fmt.Println("this is defer")
+// 	lenght = len(name)
+// 	uppercase = strings.ToUpper(name)
+// 	return
+// }
+
+// func main() {
+// 	totalLenght, up := lenAn("nico")
+// 	fmt.Println(totalLenght, up)
+// }
