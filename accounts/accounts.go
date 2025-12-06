7e296d969ef87598c56dec843d4b81efad1d1a17
go-up-lang <dbsrbtjs@gmail.com>
2025-12-07 02:01:05 +0900

    Bank, Accounts complete

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,11 +1,33 @@
 package accounts
 
+import "errors"
+
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw")
+
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// 입금
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// 출금
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+func (a Account) Balance() int {
+	return a.balance
 }
--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	account := accounts.NewAccount("gyusun")
-	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(15)
+	if err != nil {
+		fmt.Println("error 내용 ", err)
+	}
+	fmt.Println(account.Balance())
+
 }
