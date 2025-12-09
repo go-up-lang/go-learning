d0a9e7cb01617a59f76cec05918d28c78c2a9ed8
go-up-lang <dbsrbtjs@gmail.com>
2025-12-09 16:33:27 +0900

    Dictionary part one

--- main.go
+++ main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"fmt"
 
-	"example.com/banking/accounts"
+	"example.com/banking/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("gyusun")
-	account.Deposit(10)
-	// fmt.Println(account.Balance())
-	err := account.Withdraw(15)
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
 	if err != nil {
-		fmt.Println("error 내용 ", err)
+		fmt.Println(err)
 	}
 
-	fmt.Println(account)
-	// fmt.Println(account.Balance(), account.Owner())
+	definitionValue, _ := dictionary.Search(word)
+	fmt.Println(definitionValue)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(definition)
 
 }
---
+++ mydict/mydict.go
@@ -0,0 +1,31 @@
+package mydict
+
+import (
+	"errors"
+)
+
+type Dictionary map[string]string
+
+var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
+
+func (d Dictionary) Search(word string) (string, error) {
+
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
